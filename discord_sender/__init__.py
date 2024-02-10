e36181bfa2143efabfe759b1efd2beaaba674075
Jacob <112590190+Jacoblightning@users.noreply.github.com>
2024-02-10 13:33:02 -0500

    Add a backup EULA function

--- discord_sender/__init__.py
+++ discord_sender/__init__.py
@@ -2,6 +2,32 @@ import os
 
 import pymsgbox
 
+
+def _tk_warning():
+    if (
+            pymsgbox.confirm(
+                "This program breaks Discords TOS.\nPress yes to aknowledge all responsibility\nThis will be the only "
+                "time this is asked.",
+                buttons=["Yes, I accept", "No"],
+            )
+            == "No"
+    ):
+        exit(0)
+    _eula_accepted()
+
+
+def _term_warning():
+    if not input("This program breaks Discords TOS.\nPress yes to aknowledge all responsibility\nThis will be the "
+                 "only time this is asked. [N/y]").lower().startswith("y"):
+        exit(0)
+    _eula_accepted()
+
+
+def _eula_accepted():
+    with open(os.path.join(cfg, ".eulaaccepted"), "a") as f:
+        pass
+
+
 CONFIG = {
     "nt": os.path.expanduser("~\\AppData\\Local\\discord-sender"),
     "other": os.path.expanduser("~/.config/discord-sender"),
@@ -10,15 +36,13 @@ cfg = CONFIG["nt"] if os.name == "nt" else CONFIG["other"]
 if not os.path.exists(os.path.join(cfg, ".eulaaccepted")):
     if not os.path.exists(cfg):
         os.mkdir(cfg)
-    if (
-        pymsgbox.confirm(
-            "This program breaks Discords TOS.\nPress yes to aknowledge all responsibility\nThis will be the only "
-            "time this is asked.",
-            buttons=["Yes, I accept", "No"],
-        )
-        == "No"
-    ):
-        exit(0)
-    with open(os.path.join(cfg, ".eulaaccepted"), "a") as f:
-        pass
+    try:
+        _tk_warning()
+    except Exception:
+        try:
+            _term_warning()
+        except Exception:
+            print("You cannot use this program until you have accepted the eula but there was an error writing the "
+                  "config file.\n To continue, please run \"python -m discord_sender --accept-eula\" as "
+                  "administrator/root")
     from . import discord
---
+++ discord_sender/__main__.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+if "--accept-eula" in sys.argv:
+    CONFIG = {
+        "nt": os.path.expanduser("~\\AppData\\Local\\discord-sender"),
+        "other": os.path.expanduser("~/.config/discord-sender"),
+    }
+    cfg = CONFIG["nt"] if os.name == "nt" else CONFIG["other"]
+    with open(os.path.join(cfg, ".eulaaccepted"), "a") as f:
+        pass
+    os.chmod(os.path.join(cfg, ".eulaaccepted"), 0o777)
+    print("The eula was successfully accepted")
+    sys.exit(0)
+print("usage: python -m discord_sender --accept-eula\nOnly use this if the import hook fails.")
