005cd262677d345c22f14426dd255e53ca96c464
Jacob <112590190+Jacoblightning@users.noreply.github.com>
2024-02-10 12:06:54 -0500

    Fixed typing and added EULA

--- discord_sender/__init__.py
+++ discord_sender/__init__.py
@@ -1,1 +1,24 @@
-from .discord import *
+import os
+
+import pymsgbox
+
+CONFIG = {
+    "nt": os.path.expanduser("~\\AppData\\Local\\discord-sender"),
+    "other": os.path.expanduser("~/.config/discord-sender"),
+}
+cfg = CONFIG["nt"] if os.name == "nt" else CONFIG["other"]
+if not os.path.exists(os.path.join(cfg, ".eulaaccepted")):
+    if not os.path.exists(cfg):
+        os.mkdir(cfg)
+    if (
+        pymsgbox.confirm(
+            "This program breaks Discords TOS.\nPress yes to aknowledge all responsibility\nThis will be the only "
+            "time this is asked.",
+            buttons=["Yes, I accept", "No"],
+        )
+        == "No"
+    ):
+        exit(0)
+    with open(os.path.join(cfg, ".eulaaccepted"), "a") as f:
+        pass
+    from . import discord
--- discord_sender/discord.py
+++ discord_sender/discord.py
@@ -1,5 +1,6 @@
-import requests
 import copy
+
+import requests
 
 
 class InvalidCredentialsException(Exception):
@@ -11,17 +12,19 @@ class ArgumentError(Exception):
 
 
 class DiscordLoginInfo:
-    def __init__(self, *, token: str = None, cookie=None, uid: str = None):
+    def __init__(
+        self, *, token: str | None = None, cookie: None = None, uid: str | None = None
+    ):
         if not token:  # Keep it until we have cookie auth
             raise ArgumentError("Token must be provided")
         if not (token or cookie):
             raise ArgumentError("either token or cookie is required")
-        self.__token = token
-        self.__cookie = cookie
-        self.uid = uid
-        self.preferred_method = "token"
+        self.__token: str | None = token
+        self.__cookie: None = cookie
+        self.uid: str | None = uid
+        self.preferred_method: str = "token"
 
-    def get_token(self):
+    def get_token(self) -> str | None:
         return self.__token
 
 
@@ -39,31 +42,34 @@ class DiscordUser:
         self.__logged_in: bool = False
         self.session: requests.Session | None = None
 
-    def login_with_credentials(self, email: str, password: str):
+    def login_with_credentials(self, email: str, password: str) -> None:
         self.session = requests.Session()
         self.session.get("https://discord.com/login")  # Get required cookie
         creds = copy.copy(self.DATA)  # Get a copy of the default data
         creds["login"] = email
         creds["password"] = password
-        response = self.session.post(
+        response: requests.Response = self.session.post(
             "https://discord.com/api/v9/auth/login", headers={}, json=creds
         )
         if not response.ok:
-            print(response.json())
             error: dict[str, str] = response.json()["errors"]["login"]["_errors"][0]
             if error["code"] == "INVALID_LOGIN":
                 raise InvalidCredentialsException(error["message"])
-            raise requests.RequestException(f"Unknown error code from discord: {error['code']}")
+            raise requests.RequestException(
+                f"Unknown error code from discord: {error['code']}"
+            )
         user_data: dict[str, str | dict[str, str]] = response.json()
-        self.user_info = DiscordLoginInfo(token=user_data["token"], uid=user_data["user_id"])
-        self.__logged_in = True
+        self.user_info = DiscordLoginInfo(
+            token=user_data["token"], uid=user_data["user_id"]
+        )
+        self.__logged_in: bool = True
 
-    def login_with_token(self, token):
+    def login_with_token(self, token) -> None:
         self.user_info = DiscordLoginInfo(token=token)
         self.__logged_in = True
         self.session = requests.Session()
 
-    def login_with_cookie(self, cookie):
+    def login_with_cookie(self, cookie: None):
         raise NotImplementedError("Cookie not implemented yet")  # TODO: Figure this out
 
     def send_message_to_channel(self, message: str, channel_id: str):
@@ -73,20 +79,28 @@ class DiscordUser:
             raise NotImplementedError("Only token authentication can be used currently")
         heads = {"Authorization": self.user_info.__token}
         json_data = {
-            'mobile_network_type': 'unknown',
-            'content': message,
-            'nonce': '0',  # For some reason this works as intended??
+            "mobile_network_type": "unknown",
+            "content": message,
+            "nonce": "0",  # For some reason this works as intended??
             # https://github.com/discord/discord-api-docs/issues/5607
-            'tts': False,
-            'flags': 0,
+            "tts": False,
+            "flags": 0,
         }
-        self.session.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", headers=heads, data=json_data)
+        self.session.post(
+            f"https://discord.com/api/v9/channels/{channel_id}/messages",
+            headers=heads,
+            data=json_data,
+        )
 
-    def get_channel_id(self, user_id: str):
+    def get_channel_id(self, user_id: str) -> str:
+        if not self.__logged_in:
+            raise InvalidCredentialsException("You need to login first")
         data = {"recipient_id": user_id}
-        headers = {"authorization": self.user_info.__token}
-        response = requests.post(f'https://discord.com/api/v9/users/@me/channels', json=data, headers=headers)
-        return response.json()['id']
+        headers: dict[str, str | None] = {"authorization": self.user_info.__token}
+        response = requests.post(
+            f"https://discord.com/api/v9/users/@me/channels", json=data, headers=headers
+        )
+        return response.json()["id"]
 
-    def send_message_to_user(self, message: str, user_id: str):
+    def send_message_to_user(self, message: str, user_id: str) -> None:
         self.send_message_to_channel(message, self.get_channel_id(user_id))
