188d7888214c54d4b43724b473dd0a2113c9a595
Jacob <112590190+Jacoblightning@users.noreply.github.com>
2024-02-15 07:32:05 -0500

    Take away unnecessary dependencies.

--- discord_sender/__init__.py
+++ discord_sender/__init__.py
@@ -5,20 +5,6 @@ import requests
 import sys
 
 if "--accept-eula" not in sys.argv:
-    def _tk_warning():
-        if (
-                pymsgbox.confirm(
-                    "This program breaks Discords TOS.\nPress yes to aknowledge all responsibility\nThis will be the "
-                    "only"
-                    "time this is asked.",
-                    buttons=["Yes, I accept", "No"],
-                )
-                == "No"
-        ):
-            exit(0)
-        _eula_accepted()
-
-
     def _term_warning():
         if (
                 not input(
@@ -46,14 +32,11 @@ if "--accept-eula" not in sys.argv:
         if not os.path.exists(cfg):
             os.mkdir(cfg)
         try:
-            _tk_warning()
+            _term_warning()
         except Exception:
-            try:
-                _term_warning()
-            except Exception:
-                print(
-                    "You cannot use this program until you have accepted the eula but there was an error writing the "
-                    'config file.\n To continue, please run "python -m discord_sender --accept-eula" as '
-                    "administrator/root"
-                )
-                exit(1)
+            print(
+                "You cannot use this program until you have accepted the eula but there was an error writing the "
+                'config file.\n To continue, please run "python -m discord_sender --accept-eula" as '
+                "administrator/root"
+            )
+            exit(1)
