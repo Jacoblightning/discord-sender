bade5921e69a7b594754b38dc9af70c9189dfb59
Jacob <112590190+Jacoblightning@users.noreply.github.com>
2024-02-15 07:27:06 -0500

    Ugh, Fix tests...

--- discord_sender/__init__.py
+++ discord_sender/__init__.py
@@ -2,56 +2,58 @@ import os
 
 import pymsgbox
 import requests
+import sys
+
+if "--accept-eula" not in sys.argv:
+    def _tk_warning():
+        if (
+                pymsgbox.confirm(
+                    "This program breaks Discords TOS.\nPress yes to aknowledge all responsibility\nThis will be the "
+                    "only"
+                    "time this is asked.",
+                    buttons=["Yes, I accept", "No"],
+                )
+                == "No"
+        ):
+            exit(0)
+        _eula_accepted()
 
 
-def _tk_warning():
-    if (
-        pymsgbox.confirm(
-            "This program breaks Discords TOS.\nPress yes to aknowledge all responsibility\nThis will be the only "
-            "time this is asked.",
-            buttons=["Yes, I accept", "No"],
-        )
-        == "No"
-    ):
-        exit(0)
-    _eula_accepted()
+    def _term_warning():
+        if (
+                not input(
+                    "This program breaks Discords TOS.\nPress yes to aknowledge all responsibility\nThis will be the "
+                    "only time this is asked. [N/y]"
+                )
+                        .lower()
+                        .startswith("y")
+        ):
+            exit(0)
+        _eula_accepted()
 
 
-def _term_warning():
-    if (
-        not input(
-            "This program breaks Discords TOS.\nPress yes to aknowledge all responsibility\nThis will be the "
-            "only time this is asked. [N/y]"
-        )
-        .lower()
-        .startswith("y")
-    ):
-        exit(0)
-    _eula_accepted()
+    def _eula_accepted():
+        with open(os.path.join(cfg, ".eulaaccepted"), "a") as f:
+            pass
 
 
-def _eula_accepted():
-    with open(os.path.join(cfg, ".eulaaccepted"), "a") as f:
-        pass
-
-
-CONFIG = {
-    "nt": os.path.expanduser("~\\AppData\\Local\\discord-sender"),
-    "other": os.path.expanduser("~/.config/discord-sender"),
-}
-cfg = CONFIG["nt"] if os.name == "nt" else CONFIG["other"]
-if not os.path.exists(os.path.join(cfg, ".eulaaccepted")):
-    if not os.path.exists(cfg):
-        os.mkdir(cfg)
-    try:
-        _tk_warning()
-    except Exception:
+    CONFIG = {
+        "nt": os.path.expanduser("~\\AppData\\Local\\discord-sender"),
+        "other": os.path.expanduser("~/.config/discord-sender"),
+    }
+    cfg = CONFIG["nt"] if os.name == "nt" else CONFIG["other"]
+    if not os.path.exists(os.path.join(cfg, ".eulaaccepted")):
+        if not os.path.exists(cfg):
+            os.mkdir(cfg)
         try:
-            _term_warning()
+            _tk_warning()
         except Exception:
-            print(
-                "You cannot use this program until you have accepted the eula but there was an error writing the "
-                'config file.\n To continue, please run "python -m discord_sender --accept-eula" as '
-                "administrator/root"
-            )
-            exit(1)
+            try:
+                _term_warning()
+            except Exception:
+                print(
+                    "You cannot use this program until you have accepted the eula but there was an error writing the "
+                    'config file.\n To continue, please run "python -m discord_sender --accept-eula" as '
+                    "administrator/root"
+                )
+                exit(1)
